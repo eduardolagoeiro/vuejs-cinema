5389559982b72a60e5c19a1aaebc0ce851147e7e
Eduardo Lagoeiro <eduardolagoeiro@id.uff.br>
2018-10-04 16:34:00 -0300

    emmiting event

--- src/main.js
+++ src/main.js
@@ -40,16 +40,45 @@ new Vue({
         <div id="movie-filter">
           <h2>Movie Filter</h2>
           <div class="filter-group">
-            <check-filter v-for="genre in genres" v-bind:name="genre"/>
+            <check-filter
+              v-for="genre in genres"
+              v-on:check-filter="checkFilterHandler"
+              v-bind:name="genre"/>
           </div>
         </div>
       `,
+      methods: {
+        checkFilterHandler(){
+          console.log('msg');
+        }
+      },
       components: {
         'check-filter': {
+          data(){
+            return {
+              checked: false,
+            }
+          },
           props:['name'],
           template:`
-            <div>{{name}}</div>
-          `
+            <div class="check-filter"
+              v-on:click="checkFilter"
+              v-bind:class="{ active: checked }">
+              <span class="checkbox"></span>
+              <span class="check-filter-title">
+                {{name}}
+              </span>
+            </div>
+          `,
+          methods: {
+            checkFilter(){
+              this.checked = !this.checked;
+              this.$emit('check-filter', {
+                name: this.name,
+                checked: this.checked
+              });
+            }
+          }
         }
       }
     }
