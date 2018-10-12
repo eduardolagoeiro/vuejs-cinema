9c9691ee34f6f8e6e9b667d836e117f1b9b90c3b
lagoeiro <eduardolagoeiro@yahoo.com.br>
2018-10-12 16:44:30 -0300

    view router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import './style.scss';
 import Vue from 'vue';
-import Overview from './components/Overview.vue';
 
 const bus = new Vue();
 Object.defineProperty(Vue.prototype, '$bus', {get(){return this.$root.bus}});
 
 import {checkFilterHandler} from './util/bus';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter)
+
+import routes from './util/routes';
+const router = new VueRouter({routes});
 
 new Vue({
   el: '#app',
@@ -16,9 +21,9 @@ new Vue({
     bus
   },
   components:{
-    Overview
   },
   created: function(){
     this.$bus.$on('check-filter', checkFilterHandler.bind(this));
-  }
+  },
+  router
 });
---
+++ src/util/routes.js
@@ -0,0 +1,7 @@
+import Overview from '../components/Overview.vue';
+
+export default [
+    { path: '/', component: Overview },
+]
+    
+  
