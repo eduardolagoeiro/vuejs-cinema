112af2ae5bafc00c7ddf0d1ad04dfd0ea796cdca
lagoeiro <eduardolagoeiro@yahoo.com.br>
2018-10-12 11:00:52 -0300

    bus on vue prototype

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import Vue from 'vue';
 import MovieList from './components/MovieList.vue';
 import MovieFilter from './components/MovieFilter.vue';
 
+const bus = new Vue();
+Object.defineProperty(Vue.prototype, '$bus', {get(){return this.$root.bus}});
+
 new Vue({
   el: '#app',
   data: {
     genre: [],
     time: [],
-    day: (new Date()).getDate()
+    day: (new Date()).getDate(),
+    bus
   },
   methods: {
     checkFilterHandler(category, title, checked){
