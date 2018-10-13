05e886f0f36a2c706c661a67d9b307650a937776
lagoeiro <eduardolagoeiro@yahoo.com.br>
2018-10-12 23:48:57 -0300

    router to absolute path

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div class="movie-col-left">
-      <router-link v-bind:to="`movie/${movie.id}`">
+      <router-link v-bind:to="`../movie/${movie.id}`">
         <img v-bind:src="movie.poster"/>
       </router-link>
     </div>
     <div class="movie-col-right">
       <div class="movie-title">
-        <router-link v-bind:to="`movie/${movie.id}`">
+        <router-link v-bind:to="`../movie/${movie.id}`">
           <h2>{{movie.title}}</h2>
         </router-link>
         <span class="movie-rating">{{movie.rated}}</span>
